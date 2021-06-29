2d7e1143c3ee939de6baebc1535550d4fa6fc7bb
sangilyoon-dev <sangilyoon.dev@gmail.com>
2021-06-29 23:51:23 +0900

    210629 first

---
+++ Lv3/베스트앨범.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        return genres.indices.groupBy { genres[it] }
+            .toList()
+            .sortedByDescending { it.second.sumBy { plays[it] } }
+            .map { it.second.sortedByDescending { plays[it] }.take(2) }
+            .flatten()
+            .toIntArray()
+    }
+}
